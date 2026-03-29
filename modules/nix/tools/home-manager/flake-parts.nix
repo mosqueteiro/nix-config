442bc73a67f5f83d281127ebbc4e1b6027b8b936
mosqueteiro <nat3.th3.gr3at@gmail.com>
2026-03-29 16:02:04 -0600

    [cleanup] prep for cleanup

--- modules/nix/tools/home-manager/flake-parts.nix
+++ modules/nix/tools/home-manager/flake-parts.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   # Manage user environments using Nix
-  flake.inputs = {
+  flake-file.inputs = {
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
