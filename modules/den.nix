b949c75704c5f2e555b83c64e399161e342f5845
mosqueteiro <nat3.th3.gr3at@gmail.com>
2026-03-30 18:25:01 -0600

    [bluetooth] enable bluetooth

--- modules/den.nix
+++ modules/den.nix
@@ -41,14 +41,37 @@
           pkgs.btop
           pkgs.fastfetch
         ];
+
+        # Sound
+        # Enable sound with pipewire.
+        services.pulseaudio.enable = false;
+        security.rtkit.enable = true;
+        services.pipewire = {
+          enable = true;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+          # If you want to use JACK applications, uncomment this
+          #jack.enable = true;
+
+          # use the example session manager (no others are packaged yet so this is enabled by default,
+          # no need to redefine it in your config for now)
+          #media-session.enable = true;
+        };
+
+        # Bluetooth
+        hardware.bluetooth.enable = true;
+
       };
   };
 
   den.aspects.gaming = {
-    nixos = { pkgs, ... }: {
-      programs.gamescope.enable = true;
-      programs.steam.enable = true;
-    };
+    nixos =
+      { ... }:
+      {
+        programs.gamescope.enable = true;
+        programs.steam.enable = true;
+      };
   };
 
   den.aspects.mosqueteiro = {
